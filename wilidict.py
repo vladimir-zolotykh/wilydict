88af9b4319d4cd2a26d3340a3f874bdf7763d1f7
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-18 09:36:30 +0300

    Implement WilyDict, AutoMeta, AutoConst, Color
    Main guard if __name__ == '__main__;

---
+++ wilidict.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class WilyDict(dict):
+    _next: int = 0
+
+    def __missing__(self, key):
+        if key.startswith("__") and key.endswith("__"):
+            pass
+        else:
+            self[key] = self._next
+            self._next += 1
+
+
+class AutoMeta(type):
+    def __prepare__(cls, bases, **kwds):
+        # cls: AutoConst
+        return WilyDict()
+
+
+class AutoConst(metaclass=AutoMeta):
+    pass
+
+
+class Color(AutoConst):
+    RED
+    GREEN
+    BLUE
+
+
+if __name__ == "__main__":
+    c = Color()
+    print(c.RED, c.GREEN, c.BLUE)
